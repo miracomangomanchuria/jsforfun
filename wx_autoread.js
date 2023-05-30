990c1fe24ac62fc35cb85bd8d42ceeac269e2d7b
miracomangomanchuria <591757131@qq.com>
2023-05-30 09:22:25 +0800

    Create wx_autoread.js

---
+++ wx_autoread.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https?://mp\.weixin\.qq\.com/s.+? url response-body </script> response-body let scrollInterval = setInterval(() => {window.scrollBy(0, window.innerHeight);}, 3000); setTimeout(() => { clearInterval(scrollInterval); window.history.back();  }, 11000); </script>
+
+[mitm]
+hostname = mp.weixin.qq.com
